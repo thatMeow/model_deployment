4b11d252fdab1f39b63b5a8d760cca17baad430d
thatMeow <thatMeow@users.noreply.github.com>
2019-07-19 13:05:10 -0400

    Create transformer_classes.py

---
+++ pipelines/transformer_classes.py
@@ -0,0 +1,294 @@
+class DFFunctionTransformer(TransformerMixin, BaseEstimator):
+    # FunctionTransformer but for pandas DataFrames
+
+    def __init__(self, *args, **kwargs):
+        self.ft = FunctionTransformer(*args, **kwargs)
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        Xt = self.ft.transform(X)
+        Xt = pd.DataFrame(Xt, index=X.index, columns=X.columns)
+        return Xt
+
+
+class DFFeatureUnion(TransformerMixin, BaseEstimator):
+    # FeatureUnion but for pandas DataFrames
+
+    def __init__(self, transformer_list):
+        self.transformer_list = transformer_list
+
+    def fit(self, X, y=None):
+        for (name, t) in self.transformer_list:
+            t.fit(X, y)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xts = [t.transform(X) for _, t in self.transformer_list]
+        Xunion = reduce(lambda X1, X2: pd.merge(X1, X2, left_index=True, right_index=True), Xts)
+        return Xunion
+
+
+class DFImputer(TransformerMixin, BaseEstimator):
+    # Imputer but for pandas DataFrames
+
+    def __init__(self, strategy='mean'):
+        self.strategy = strategy
+        self.imp = None
+        self.statistics_ = None
+
+    def fit(self, X, y=None):
+        self.imp = Imputer(strategy=self.strategy)
+        self.imp.fit(X)
+        self.statistics_ = pd.Series(self.imp.statistics_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Ximp = self.imp.transform(X)
+        Xfilled = pd.DataFrame(Ximp, index=X.index, columns=X.columns)
+        return Xfilled
+
+
+class DFStandardScaler(TransformerMixin, BaseEstimator):
+    # StandardScaler but for pandas DataFrames
+
+    def __init__(self):
+        self.ss = None
+        self.mean_ = None
+        self.scale_ = None
+
+    def fit(self, X, y=None):
+        self.ss = StandardScaler()
+        self.ss.fit(X)
+        self.mean_ = pd.Series(self.ss.mean_, index=X.columns)
+        self.scale_ = pd.Series(self.ss.scale_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xss = self.ss.transform(X)
+        Xscaled = pd.DataFrame(Xss, index=X.index, columns=X.columns)
+        return Xscaled
+
+
+class DFRobustScaler(TransformerMixin, BaseEstimator):
+    # RobustScaler but for pandas DataFrames
+
+    def __init__(self):
+        self.rs = None
+        self.center_ = None
+        self.scale_ = None
+
+    def fit(self, X, y=None):
+        self.rs = RobustScaler()
+        self.rs.fit(X)
+        self.center_ = pd.Series(self.rs.center_, index=X.columns)
+        self.scale_ = pd.Series(self.rs.scale_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xrs = self.rs.transform(X)
+        Xscaled = pd.DataFrame(Xrs, index=X.index, columns=X.columns)
+        return Xscaled
+
+
+class ColumnExtractor(TransformerMixin, BaseEstimator):
+
+    def __init__(self, cols):
+        self.cols = cols
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xcols = X[self.cols]
+        return Xcols
+
+
+class ZeroFillTransformer(TransformerMixin, BaseEstimator):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xz = X.fillna(value=0)
+        return Xz
+
+
+class Log1pTransformer(TransformerMixin, BaseEstimator):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xlog = np.log1p(X)
+        return Xlog
+
+
+class DateFormatter(TransformerMixin, BaseEstimator):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xdate = X.apply(pd.to_datetime)
+        return Xdate
+
+
+class DateDiffer(TransformerMixin, BaseEstimator):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        beg_cols = X.columns[:-1]
+        end_cols = X.columns[1:]
+        Xbeg = X[beg_cols].as_matrix()
+        Xend = X[end_cols].as_matrix()
+        Xd = (Xend - Xbeg) / np.timedelta64(1, 'D')
+        diff_cols = ['->'.join(pair) for pair in zip(beg_cols, end_cols)]
+        Xdiff = pd.DataFrame(Xd, index=X.index, columns=diff_cols)
+        return Xdiff
+
+
+class DummyTransformer(TransformerMixin, BaseEstimator):
+
+    def __init__(self):
+        self.dv = None
+
+    def fit(self, X, y=None):
+        # assumes all columns of X are strings
+        Xdict = X.to_dict('records')
+        self.dv = DictVectorizer(sparse=False)
+        self.dv.fit(Xdict)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xdict = X.to_dict('records')
+        Xt = self.dv.transform(Xdict)
+        cols = self.dv.get_feature_names()
+        Xdum = pd.DataFrame(Xt, index=X.index, columns=cols)
+        # drop column indicating NaNs
+        nan_cols = [c for c in cols if '=' not in c]
+        Xdum = Xdum.drop(nan_cols, axis=1)
+        return Xdum
+
+
+class MultiEncoder(TransformerMixin, BaseEstimator):
+    # Multiple-column MultiLabelBinarizer for pandas DataFrames
+
+    def __init__(self, sep=','):
+        self.sep = sep
+        self.mlbs = None
+
+    def _col_transform(self, x, mlb):
+        cols = [''.join([x.name, '=', c]) for c in mlb.classes_]
+        xmlb = mlb.transform(x)
+        xdf = pd.DataFrame(xmlb, index=x.index, columns=cols)
+        return xdf
+
+    def fit(self, X, y=None):
+        Xsplit = X.applymap(lambda x: x.split(self.sep))
+        self.mlbs = [MultiLabelBinarizer().fit(Xsplit[c]) for c in X.columns]
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xsplit = X.applymap(lambda x: x.split(self.sep))
+        Xmlbs = [self._col_transform(Xsplit[c], self.mlbs[i])
+                 for i, c in enumerate(X.columns)]
+        Xunion = reduce(lambda X1, X2: pd.merge(X1, X2, left_index=True, right_index=True), Xmlbs)
+        return Xunion
+
+
+class StringTransformer(TransformerMixin, BaseEstimator):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xstr = X.applymap(str)
+        return Xstr
+
+
+class ClipTransformer(TransformerMixin, BaseEstimator):
+
+    def __init__(self, a_min, a_max):
+        self.a_min = a_min
+        self.a_max = a_max
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xclip = np.clip(X, self.a_min, self.a_max)
+        return Xclip
+
+
+class AddConstantTransformer(TransformerMixin, BaseEstimator):
+
+    def __init__(self, c=1):
+        self.c = c
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xc = X + self.c
+        return Xc
+
+
+class MultiColumnLabelEncoder(TransformerMixin, BaseEstimator):  
+    """Transformer for applying label encoder on multiple columns.
+
+    This transformer applies label encoding to columns in a dataset.
+    """
+
+    def __init__(self):
+        self.d = defaultdict(LabelEncoder)
+
+    def transform(self, X, **transform_params):
+        """Transforms X to have columns label encoded.
+
+        Args:
+            X (obj): The dataset to transform. Can be dataframe or matrix.
+            transform_params (kwargs, optional): Additional params.
+
+        Returns:
+            The transformed dataset with the label encoded columns.
+        """
+        X = X.fillna('NaN')  # fill null values with 'NaN'
+        transformed = X.apply(lambda x: self.d[x.name].transform(x))
+        return transformed
+
+    def fit(self, X, y=None, **fit_params):
+        """Fits transfomer over X.
+
+        Needs to apply fit over the defaultdict so as to retain the
+        label classes when transforming.
+        """
+        X = X.fillna('NaN')  # fill null values with 'NaN'
+        X.apply(lambda x: self.d[x.name].fit(x))
+        return self
